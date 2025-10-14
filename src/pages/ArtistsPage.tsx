14c3d96dd73fe4b0bc7e34e6d5e53f61fd0f20e0
djnightsl4yer <151681527+djnightsl4yer@users.noreply.github.com>
2025-10-14 21:08:16 +0200

    Updated ArtistsPage.tsx

--- src/pages/ArtistsPage.tsx
+++ src/pages/ArtistsPage.tsx
@@ -65,7 +65,7 @@ const artists: Artist[] = [
     name: 'C0R3',
     role: 'DJ',
     bio: 'DJ et producteur émergent de la scène techno parisienne, C0R3 s\'impose avec son style énergique et ses sélections pointues. Régulièrement programmé dans des événements underground à Paris, notamment au Studio 56, il délivre des sets intenses qui captivent les foules. Son énergie brute et son approche sans concession font de chaque performance un moment marquant de la nuit.',
-    image: '/insta rumbl.jpg',
+    image: '/cdj yugi.jpg',
     genre: 'Techno',
     links: {
       shotgun: 'https://shotgun.live/en/artists/c0r3dj',
