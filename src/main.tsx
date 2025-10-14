02b9aa375f7bf1cf673fd048f062a3ee02a5bbd9
djnightsl4yer <151681527+djnightsl4yer@users.noreply.github.com>
2025-10-14 17:48:14 +0200

    Updated tsconfig.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 import './index.css';
+import { trackRefCode } from './utils/trackRefCode';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+trackRefCode();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>
 );
